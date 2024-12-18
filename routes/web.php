16dc25614ebb8b6d38edca16b196aa64a78422d8
Clévison barbosa <50533700+clevisonbarbosa@users.noreply.github.com>
2024-12-17 21:19:10 -0300

    Update Welcome

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('tasks.index');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
